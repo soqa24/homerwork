c65eaa1149fbd418aeb5691f0d99d58c8183ab46
soqa24 <aleksey.marihin@gmail.com>
2025-08-05 00:47:26 +0300

    Add files via upload

--- урок-150/js/main.js
+++ урок-150/js/main.js
@@ -168,6 +168,13 @@
             }
         },
     });
+
+    // Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
     
 
 })()
