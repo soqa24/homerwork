7154a17be176f74c5e9b2e2ef8a3cb647c241e64
soqa24 <aleksey.marihin@gmail.com>
2026-01-11 17:40:11 +0400

    Add files via upload

---
+++ дз-10/main.js
@@ -0,0 +1,28 @@
+// Задача 1
+const firstName = 'Max';
+const lastName = 'Smit';
+const isStudent = true;
+
+// ------------------
+
+// Задача 2 
+
+let age = 19;
+let currentYaer = 2026;
+
+const birthYear = currentYaer - age;
+console.log(birthYear);
+
+//Задача 3 
+
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`);
+
+// Задача 4 
+
+let a = '123'; //будет строка
+let b = +'123'; // будет number
+let c = Number('789'); //будет number
+let d = Boolean(0); //будет false  
+let e = Boolean(' '); // будет true
+let result = a + b + c + d + e; // будет строка
+
