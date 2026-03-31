a94e936a984848379445e7e2ec66aef6bfdafb0f
soqa24 <aleksey.marihin@gmail.com>
2026-03-31 23:03:26 +0400

    Add files via upload

---
+++ js-67/main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+
+// {
+//     id: 1,
+//     text: "Todo",
+//     is_completed:fals,
+// }
+const todoKeys = {
+    id: "id",
+    text: "text",
+    is_completed: "is_completed",
+};
+
+
+let todos = [];
+
+const errTodoNotFound = todoId => `Todo woth id ${todoId} not found`;
+
+const getNewTodoId = todos => todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+    const newTodo = {
+        [todoKeys.id]: getNewTodoId(todos),
+        [todoKeys.text]: text,
+        [todoKeys.is_completed]: false,
+    };
+    todos.push(newTodo);
+    return newTodo;
+};
+
+
+const completeTodoById = (todos, todoId) => {
+    const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+    if (todo === undefined){
+        console.error(errTodoNotFound(todoId));
+        return null;
+    }
+    todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+    return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+    const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+    if (todoIndex == -1) {
+        console.error(errTodoNotFound(todoId));
+        return todos;
+    }
+    todos.splice(todoIndex, 1);
+    return todos;
+};
+
+const updateTodoTextById = (todos, todoId, newText) => {
+    const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+
+    if (todo === undefined) {
+        console.error(errTodoNotFound(todoId));
+        return null
+    }
+    todo[todoKeys.text] = newText;
+    return todo;
+}
+// const deleteTodoById = (todos, todoId) => {
+//     return todos.filter((todo) => todo[todoKeys.id] !== todoId);
+// };
